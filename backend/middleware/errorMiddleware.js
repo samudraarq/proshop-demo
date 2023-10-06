6281806045006ccfa95878966e949c890be3190a
Samudra Faris Arqam <samudra@acalution.com>
2023-10-06 09:54:50 +0700

    errorhandler and controller

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,29 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  // call next() with error to trigger error handler
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  // if status code is 200, set to 500
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  // check for mongoose bad ObjectId
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    message = "Resource not found";
+    statusCode = 404;
+  }
+
+  // set status code
+  res.status(statusCode);
+  // return error message
+  res.json({
+    message: err.message,
+    // only show stack trace in development
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };

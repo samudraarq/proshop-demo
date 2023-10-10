87a7ff45fcded22979d32ef515aa04b23d04ccfb
Samudra Faris Arqam <samudra@acalution.com>
2023-10-10 09:25:41 +0700

    logout function

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -14,12 +14,12 @@ const authSlice = createSlice({
       state.userInfo = action.payload;
       localStorage.setItem("userInfo", JSON.stringify(action.payload));
     },
-    removeCredentials: (state) => {
+    logout: (state, action) => {
       state.userInfo = null;
       localStorage.removeItem("userInfo");
     },
   },
 });
 
-export const { setCredentials, removeCredentials } = authSlice.actions;
+export const { setCredentials, logout } = authSlice.actions;
 export default authSlice.reducer;

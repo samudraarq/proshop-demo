b7fa8188720554506f981debc2e0e34b09407a56
Samudra Faris Arqam <samudra@acalution.com>
2023-10-23 10:51:35 +0700

    user list screen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -129,28 +129,67 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @route   GET /api/users
 // @access  Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("get users");
+  const users = await User.find({}); // Find all users
+  res.status(200).json(users); // Send users data
 });
 
 // @desc    Get user by ID
 // @route   GET /api/users/:id
 // @access  Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
-  res.send("get user by id");
+  const user = await User.findById(req.params.id).select("-password"); // Find user by ID
+  if (user) {
+    res.status(200).json(user); // Send user data
+  } else {
+    res.status(404); // Not found
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Delete user
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("delete user");
+  const user = await User.findById(req.params.id); // Find user by ID
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400); // Bad request
+      throw new Error("Cannot delete admin user");
+    } else {
+      await User.deleteOne({
+        _id: user._id,
+      });
+      res.status(200).json({ message: "User deleted" }); // Send success message
+    }
+  } else {
+    res.status(404); // Not found
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Update user
 // @route   PUT /api/users/:id
 // @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("update user");
+  const user = await User.findById(req.params.id); // Find user by ID
+  if (user) {
+    user.name = req.body.name || user.name; // Update user name
+    user.email = req.body.email || user.email; // Update user email
+    user.isAdmin = Boolean(req.body.isAdmin); // Update user admin status
+
+    const updatedUser = await user.save(); // Save updated user
+
+    res.status(200).json({
+      // Send updated user data
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404); // Not found
+    throw new Error("User not found");
+  }
 });
 
 export {

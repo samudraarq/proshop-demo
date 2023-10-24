8deb512a914d38fc49ad2104584e49e0d19ead24
Samudra Faris Arqam <samudra@acalution.com>
2023-10-24 14:13:15 +0700

    production setup

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -38,6 +34,20 @@ app.get("/api/config/paypal", (req, res) =>
 // Make uploads folder static
 const __dirname = path.resolve(); // __dirname = current directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  // Set frontend build folder as static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // Serve index.html file if any route is not found
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 // Error handling
 app.use(notFound);
